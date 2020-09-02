9a053ccbb80868068981007c8494ea7ac7d9787c
Hank2400 <70624916+Hank2400@users.noreply.github.com>
2020-09-02 11:55:12 +0300

    Update main.cpp

--- for№7/main.cpp
+++ for№7/main.cpp
@@ -1,16 +1,16 @@
- #include <iostream>
+ #include <iostream>         // Джибраилов \/ Завадский
 
 using namespace std;
 
 int main()
 {
-    int a,b,s,i;
-    cin>>a>>b;
-    s=0;
-    for(i=a;i<b+1;i++)
+    int a,b,s,i; // обьявление переменных
+    cin>>a>>b; // вводим а,б
+    s=0; // при равниваем s к 0 т.к. изначальное значения 1
+    for(i=a;i<b+1;i++) 
     {
         s+=i;
     }
-    cout<< s;
+    cout<< s; // выводим
     return 0;
 }
