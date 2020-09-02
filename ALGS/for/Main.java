11b944f66191eb62e69c83955dabbb7bf0713b57
fifepls <64140873+fifepls@users.noreply.github.com>
2020-09-02 09:40:20 +0300

    Add files via upload

---
+++ ALGS/for/Main.java
@@ -0,0 +1,13 @@
+//For1. Даны целые числа K и N (N > 0). Вывести N раз число K.
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int k = 5, n = 10; // k число выводов n количество выводов
+            for(int i = 0; i < n; ++i){
+                System.out.println(k);// вывод
+            }
+    }
+}
