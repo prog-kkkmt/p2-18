7ba26feb6555f389ec31787e6f112d27ae6095c6
DanskyAM <70624810+DanskyAM@users.noreply.github.com>
2020-09-02 12:07:31 +0300

    Add files via upload

---
+++ Данский, Михайлов/If6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+float a,b;//объ€вл€ем 2 числа
+    
+scanf("%f%f", &a, &b);//ввобим числа
+    
+if (a>b)
+    
+printf("%.2f", a);//если a>b, ввыводим a
+    
+else printf("%.2f", b);//в ином случае ввыводим b
+
+    
+return 0;
+}
