0a3e5482a9e37ef478c89c8134a8dbdce61c16cf
emptyzach <64543798+emptyzach@users.noreply.github.com>
2020-09-02 11:50:45 +0300

    Add files via upload

---
+++ Алгоритмы/if 2.cpp
@@ -0,0 +1,24 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+	{
+	int a,b;
+	
+	cout << "Enter a: ";
+	cin >> a;
+	if (a>0)
+		cout << a - 8 << endl;
+	else
+	{
+		b = a + 6;
+		cout << b << endl;
+		getch();
+	}
+		getch();
+	
+	
+	}
