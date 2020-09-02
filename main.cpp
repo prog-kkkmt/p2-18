857344e51091d500f134aa9b9d10fd2c72a1df96
Hank2400 <70624916+Hank2400@users.noreply.github.com>
2020-09-02 11:52:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+#include <iostream>          //Завадский /\ Джабраилов
 using namespace std;
 
 int main()
 {
-    int a=0,b=0;
-    cin>>a>>b;
-    if(a>b)
-    cout<<1;
-    else
-    cout<<2;
+    int a=0,b=0; // обьявление переменных
+    cin>>a>>b;// ввод
+    if(a>b)// если a>b
+    cout<<1;// выводим 1
+    else // тогда
+    cout<<2; //выводим 2
 
     return 0;
 }
