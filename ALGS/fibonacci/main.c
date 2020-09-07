befa51e131eb43288c07e7bb382cb83fed1c867e
fifepls <64140873+fifepls@users.noreply.github.com>
2020-09-07 14:15:17 +0300

    Add files via upload

--- ALGS/fibonacci/main.c
+++ ALGS/fibonacci/main.c
@@ -4,15 +4,15 @@
 
 int counter = 0;
 
-
 int fib(int n){
     ++counter;
 
-if(n == 1  || n == 2){
-    return 1;
-}
+    if (n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
 
-return fib(n - 1) + fib(n - 2);
+    return fib(n - 1) + fib(n - 2);
 }
 
 int main()
