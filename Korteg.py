179d57dd61f10ed1473bb3daf71d30c6feb39474
Mental41 <64136709+Mental41@users.noreply.github.com>
2020-09-15 10:26:31 +0300

    Add files via upload

---
+++ Korteg.py
@@ -0,0 +1,9 @@
+a = tuple((1, 2, 3, 4))
+b = tuple((5, 6, 7, 8))
+c = a + b
+print(a)
+print(b)
+print(c)
+
+
+
---
+++ mnogestvo.py
@@ -0,0 +1,4 @@
+spisok = input().split()
+mnogestvo = set(spisok)
+dlina = len (mnogestvo)
+print(dlina)
