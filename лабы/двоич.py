528555f24bc011f750e5f46a4c0905cd52c212a1
liners21 <64136738+liners21@users.noreply.github.com>
2020-10-20 18:58:55 +0300

    Add files via upload

--- лабы/двоич.py
+++ лабы/двоич.py
@@ -1,9 +1,21 @@
-text = open('четнечет.txt', 'w')
-a = list(map(int,input().split()))
-x=0
-while len(a)>x: #for i in range(len(a))
-    if a[x] % 2 != 0:
-        a[x] = -a[x]
-    x+=1
-text.write(str(a))
-text.close()
+a=list()
+b=int(input('Укажите количество поступающих чисел: '))
+c=0
+while c<b:
+    e=input()
+    a.append(e)
+    c+=1
+f = ' '.join(a)
+with open("четнечет1.txt", "w") as file:
+    file.write(f)
+with open("четнечет1.txt", "r") as file:
+    ref = file.read()
+ref=ref.split(' ')
+for i in range(len(ref)):
+    if int(ref[i]) % 2 == 1:
+        ref[i]=int(ref[i])*-1
+        ref[i]=str(ref[i])
+f = ' '.join(ref)
+with open("четнечет2.txt", "w") as file:
+    file.write(f)
+
