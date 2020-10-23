a61ba143a9963ab43e2b77e8d02be37045fcf41e
Иван <64137168+ivauuka99@users.noreply.github.com>
2020-10-23 14:14:48 +0300

    Update File2.c

--- С/File2.c
+++ С/File2.c
@@ -1,15 +1,18 @@
+File2°. Дано имя файла и целое число N (> 1). Создать файл целых чисел с данным именем и записать в него N первых положительных четных чисел (2, 4, …).
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #define N 30
 
 int main(){
-    int num;
+        int num;
         char word[N];
         gets(word);
         scanf("%d", &num);
-        strcat(word, ".txt");
-        FILE *file2 = fopen(word, "wb+");
+            strcat(word, ".txt");
+            FILE *file2 = fopen(word, "wb+");
         for (int i = 2; i < num; i += 2)
             if ((i % 2) == 0){
                 fprintf(file2,"%d ", i);
