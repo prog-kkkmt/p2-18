c0c522ef45c7c89ac7e487e8da25ac9de7b57357
Иван <64137168+ivauuka99@users.noreply.github.com>
2020-10-23 14:14:10 +0300

    Add files via upload

---
+++ С/File2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define N 30
+
+int main(){
+    int num;
+        char word[N];
+        gets(word);
+        scanf("%d", &num);
+        strcat(word, ".txt");
+        FILE *file2 = fopen(word, "wb+");
+        for (int i = 2; i < num; i += 2)
+            if ((i % 2) == 0){
+                fprintf(file2,"%d ", i);
+        }
+        fclose(file2);
+    return 0;
+}
