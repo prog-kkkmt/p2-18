20d6a09bcb51ec52994f208ba094864681a7b90a
Иван <64137168+ivauuka99@users.noreply.github.com>
2020-10-23 13:31:19 +0300

    Update File1.c

--- С/File1.c
+++ С/File1.c
@@ -1,24 +1,25 @@
 File1. Дана строка S. Если S является допустимым именем файла, то создать пустой файл с этим именем и вывести True. Если файл с именем S создать нельзя, то вывести False.
 	
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#define N 30
 
 int main(){
-    char symb[] = "\\/*:'?><|";
-    char word[30];
-    char *a;
+    char *a, word[N], symb[] = "/\\*:?>\"<|";
     	gets(word);
     	a = strpbrk(symb, word);
-	    	if(a == NULL){
-	    		strcat(word, ".txt");
-	    		FILE *f1 = fopen(word, "w");
-				printf("True");
-				fclose(f1);
+		if(a == NULL){
+                    printf("True");
+	    	    strcat(word, ".txt");
+	     	    FILE *file1 = fopen(word, "wb+");
+		    fclose(file1);
 	    	}
     	else printf("False");
-    	
     return 0;
 }
+
+
 
 
