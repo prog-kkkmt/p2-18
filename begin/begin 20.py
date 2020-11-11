369918423ab04469e24f258c75daf39f66785e0b
Rixsat <64136626+Rixsat@users.noreply.github.com>
2020-11-11 11:09:20 +0300

    Add files via upload

---
+++ begin/begin 20.py
@@ -0,0 +1,10 @@
+from math import *
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+
+def distance(x1, y1,x2,y2):
+    c=sqrt((x2-x1)**2+(y2-y1)**2)
+c= distance(x1,y1,x2,y2)
+print(c)
