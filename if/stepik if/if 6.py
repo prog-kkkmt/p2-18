e0c858ad39ad022b519b029b0931cf4c89aef761
Rixsat <64136626+Rixsat@users.noreply.github.com>
2020-11-13 13:58:22 +0300

    Add files via upload

---
+++ if/stepik if/if 6.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if b <= a:
+    print("2 <= 1")
+if c <= b:
+    print("3 <= 2")
+if a<b and b<c:
+    print("Все в порядке")
---
+++ if/stepik if/if 9.py
@@ -0,0 +1,11 @@
+a=float(input())
+b=float(input())
+c=(input())
+if c =="+":
+    print(a+b)
+elif c =="-":
+    print(a-b)
+elif c == "/":
+    print(a/b)
+elif c == "*":
+    print(a*b)
