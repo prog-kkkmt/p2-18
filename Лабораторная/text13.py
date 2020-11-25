f6522174f749ff0f5967859bf4425b670d413ca5
Mental41 <64136709+Mental41@users.noreply.github.com>
2020-11-25 10:25:13 +0300

    Delete text13.py

--- Лабораторная/text13.py
+++
@@ -1,6 +0,0 @@
-#Дан непустой текстовый файл. Удалить из него первую строку.
-
-with open('text13.txt', 'r') as fin:
-    data = fin.read().splitlines(True)
-with open('text13.txt', 'w') as fout:
-    fout.writelines(data[1:])
