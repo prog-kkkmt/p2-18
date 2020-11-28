5718b9589911997e982c466f13c0e9fca5012d8b
emptyzach <64543798+emptyzach@users.noreply.github.com>
2020-11-28 16:27:49 +0300

    Add files via upload

---
+++ Учебная практика/Практическое занятие 8/8_2.py
@@ -0,0 +1,17 @@
+#Pythontutor
+#https://pythontutor.ru/lessons/lists/problems/num_equal_pairs/
+#Выполнили: Воронцов А.А. Бурлаев З.С Щепкин М.В П2-18
+
+"""
+Дан список чисел. Посчитайте, сколько в нем пар элементов, равных друг другу.
+Считается, что любые два элемента, равные друг другу образуют одну пару, которую необходимо посчитать.
+"""
+
+cnt = 0
+a = [int(num) for num in input().split()]
+for i in range(len(a)-1):
+    for j in range(i+1, len(a)):
+        if a[i] == a[j]:
+            cnt += 1
+
+print(cnt)
