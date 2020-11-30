363b7940c4ded9bf6bc10e9326d7a27d60235b54
Pluxury1904 <64136774+Pluxury1904@users.noreply.github.com>
2020-11-30 13:30:32 +0300

    Add files via upload

---
+++ Практика/практическая работа 8/Задание 1.py
@@ -0,0 +1,7 @@
+#https://pythontutor.ru/lessons/lists/problems/even_elements/
+#Выполнил задание Михайлов Данила
+#Выведите все четные элементы списка.
+#При этом используйте цикл for, перебирающий элементы списка, а не их индексы!
+for num in input().split(' '):
+    if int(num) % 2 == 0:
+        print(int(num), end=' ')
---
+++ Практика/практическая работа 8/Задание 2.py
@@ -0,0 +1,16 @@
+#Выполнил задание Михайлов Данила
+#pythontutor.ru
+#https://pythontutor.ru/lessons/lists/problems/more_than_neighbours/
+#Дан список чисел. Определите, сколько в этом списке элементов,
+#которые больше двух своих соседей, и выведите количество таких элементов.
+#Крайние элементы списка никогда не учитываются, поскольку у них
+#недостаточно соседей.
+a = [int(n) for n in input().split(' ')]
+sch = 0
+for i in range(len(a)):
+    try:
+        if a[i] > a[i-1] and a[i] > a[i+1] and i != 0 and i != len(a):
+            sch += 1
+    except IndexError:
+        None
+print(sch)
