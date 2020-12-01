bf6336671aa4472738dcc71a737c68f18337d2eb
Pluxury1904 <64136774+Pluxury1904@users.noreply.github.com>
2020-12-01 12:31:59 +0300

    Add files via upload

---
+++ Практика/практическая работа 9/Задание 1.py
@@ -0,0 +1,8 @@
+#Выполнил задание Михайлов Данила
+from random import randint
+x1 = randint(2, 10)
+s = []
+for i in range(x1):    
+    r = randint(1,100)
+    s.append(r)
+print(s)
---
+++ Практика/практическая работа 9/Задание 3.py
@@ -0,0 +1,6 @@
+#Выполнил задание Михайлов Данила
+#Генерирует случайное числа в диапазоне
+from random import *         
+a = randint(1,20)            
+d = [i for i in range(a)] 
+print(d) 
