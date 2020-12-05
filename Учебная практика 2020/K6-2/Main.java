df3973d5c4768b8a74e3cce09f530c7e0db55b93
fifepls <64140873+fifepls@users.noreply.github.com>
2020-12-05 11:54:31 +0300

    Add files via upload

---
+++ Учебная практика 2020/K6-2/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+//Задание 1. Подготовить инструкцию по использованию модулей math
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println(Math.random());
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.floor(56.566456));
+        System.out.println(Math.pow(3,5));
+        System.out.println(Math.sin(543));
+        System.out.println(Math.tan(345));
+
+    }
+}
