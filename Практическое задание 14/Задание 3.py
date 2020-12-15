cff687d074284688cb8048ee0753c7993bd64a44
dartin31 <64136541+dartin31@users.noreply.github.com>
2020-12-16 01:38:57 +0300

    Add files via upload

---
+++ Практическое задание 14/Задание 3.py
@@ -0,0 +1,23 @@
+#Выполнил Зайцев Н. Е. П2-18.
+#Дана строка S и текстовый файл. Заменить в файле все пустые строки на строку S.
+
+import re
+
+def file_get_contents(path,encoding="utf-8"):
+     with open(path, "r",encoding=encoding) as f:
+         out = f.read()
+         f.close() 
+         return out
+
+
+def file_put_contents(path,value,encoding="utf-8"):
+    with open(path, "w",encoding=encoding) as f:
+        out = f.write(value)
+        f.close() 
+        return True
+
+
+txt = file_get_contents("text.txt")
+s = ' строка S '
+txt = re.sub(r'\s{2,}', ' ', txt)
+file_put_contents("text.txt",txt)
