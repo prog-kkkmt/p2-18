a2a2338b2dcc7088f31a6042bba6e869c11eff4b
Иван <kvar59@yandex.ru>
2021-01-17 00:22:36 +0300

    Update Stepik.cpp

--- Прикладное программирование/ЛЗ 04/Stepik.cpp
+++ Прикладное программирование/ЛЗ 04/Stepik.cpp
@@ -1,25 +1,31 @@
+/*Выполнили: Короленко И.Р., Кузнецов М.С., Слесарев А.М.
+  Группа: ККМТ П2-18
+*/
+
 #include <iostream>	
+
 using namespace std;	
 
 void rotate(int a[], unsigned size, int shift){	
-    while(shift!=0){	
-        int temp=a[0];	
-        for(int i=1;i < size;i++){	
-            a[i-1]=a[i];}	
-            a[size-1]=temp;shift--;}	
-return;	
+    while(shift != 0){	
+        int temp = a[0];	
+        for(int i=1; i < size; i++){	
+            a[i-1] = a[i];
+		}	
+        a[size-1] = temp;
+		shift--;
+	}	
 }	
 
-int main ()	
-{	
+int main (){	
 	int size, shift;	
-	cout << "Vvedite size i shift" << endl;	
+	cout << "Input [size] i [shift]" << endl;	
 	cin >> size >> shift;	
 	int a[size];	
-	for (int i = 0 ; i < size ; i++)	
+	for (int i = 0; i < size; i++)	
 		cin >> a[i];	
-	rotate (a,size, shift);	
-	for (int i = 0 ; i < size ; i++)	
+		rotate (a,size, shift);	
+	for (int i = 0; i < size; i++)	
 		cout << a[i];	
-
+	return 0;
 }
