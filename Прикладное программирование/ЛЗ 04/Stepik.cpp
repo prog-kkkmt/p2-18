d0cc7536fd7eaf723befd64a5383c14d9d683b69
Иван <kvar59@yandex.ru>
2021-01-17 00:15:47 +0300

    ЛЗ 04 :D

---
+++ Прикладное программирование/ЛЗ 04/Stepik.cpp
@@ -0,0 +1,25 @@
+#include <iostream>	
+using namespace std;	
+
+void rotate(int a[], unsigned size, int shift){	
+    while(shift!=0){	
+        int temp=a[0];	
+        for(int i=1;i < size;i++){	
+            a[i-1]=a[i];}	
+            a[size-1]=temp;shift--;}	
+return;	
+}	
+
+int main ()	
+{	
+	int size, shift;	
+	cout << "Vvedite size i shift" << endl;	
+	cin >> size >> shift;	
+	int a[size];	
+	for (int i = 0 ; i < size ; i++)	
+		cin >> a[i];	
+	rotate (a,size, shift);	
+	for (int i = 0 ; i < size ; i++)	
+		cout << a[i];	
+
+}
