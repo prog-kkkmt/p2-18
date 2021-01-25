b4287904022f9ba8e386152438c6423c2fcdf332
emptyzach <64543798+emptyzach@users.noreply.github.com>
2021-01-25 14:03:01 +0300

    Add files via upload

---
+++ Прикладное программирование/ЛЗ_3/st_1.cpp
@@ -0,0 +1,27 @@
+/*
+https://stepik.org/lesson/Стек-вызовов-538/step/8?unit=861
+Напишите рекурсивную функцию вычисления наибольшего общего делителя двух
+положительных целых чисел (Greatest Common Divisor, GCD).
+Для этого воспользуйтесь следующими свойствами:
+
+GCD(a, b) = GCD(b, a mod b)
+GCD(0, a) = a
+GCD(a, b) = GCD(b, a)
+*/
+#include <iostream>
+using namespace std;
+unsigned gcd(unsigned a, unsigned b)
+{
+    if (b!= 0)
+    {
+        return gcd (b, (a % b));
+    }
+    return a;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b);
+    return 0;
+}
