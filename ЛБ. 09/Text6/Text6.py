d4c83441cdf8f7274f7a81cf16c84f2e088ac44b
SparkieAG <64137055+SparkieAG@users.noreply.github.com>
2021-01-26 23:09:26 +0300

    Delete Text6.py

--- ЛБ. 09/Text6/Text6.py
+++
@@ -1,9 +0,0 @@
-# Даны два текстовых файла. Добавить в конец первого файла
-#содержимое второго файла.
-txt2 = open('text66x2.txt', 'r')
-a = txt2.read()
-txt2.close()
-txt1 = open('text66.txt', 'a')
-txt1.write(a)
-txt1.close()
-print("Готово!")
