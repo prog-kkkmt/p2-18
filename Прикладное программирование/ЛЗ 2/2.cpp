ed69acf2ce26710bccc45652135b25b39cdb8607
DelcorvuZZ <64140846+DelcorvuZZ@users.noreply.github.com>
2021-01-27 01:57:38 +0300

    Add files via upload

---
+++ Прикладное программирование/ЛЗ 2/2.cpp
@@ -0,0 +1,24 @@
+/* https://stepik.org/lesson/13377/step/7?unit=3571
+*/
+
+/*Предположим, что в программе определён макрос sqr.
+
+#define sqr(x) x * x
+
+
+Какое значение будет иметь следующее выражение?
+
+sqr(3 + 0)
+*/
+
+//выражение будет иметь значение 3, чтобы в это убедитсья напишем программу
+
+#include <iostream>
+using namespace std;
+
+#define sqr(x) x * x
+int main(){
+    cout << sqr(3 + 0);
+
+    return 0;
+}
