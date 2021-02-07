28ef3b32a4db881b079c94a5add7f4c90865c5fc
VillyFoxy <55234972+VillyFoxy@users.noreply.github.com>
2021-02-07 21:51:17 +0300

    Add files via upload

---
+++ ptaskbook/array3.pas
@@ -0,0 +1,18 @@
+﻿{Дано целое число N (> 1), а также первый член A и разность D арифметической
+прогрессии. Сформировать и вывести массив размера N, содержащий N первых членов
+данной прогрессии}
+uses crt;
+var
+a:array[1..100] of integer;
+n, i, b:integer;
+begin
+  textcolor(9);
+  readln(n);
+  readln(b);
+  for i:= 1 to n do
+  begin
+    readln(a[i]);
+    inc(a[i], b);
+  end;
+  writeln(a);
+end.
