bccd012071ddaa8ae0fa05295ee1eeae9f688e81
MishaHihka <64591297+MishaHihka@users.noreply.github.com>
2021-02-08 14:11:45 +0300

    Add files via upload

---
+++ PascalABC/string/string 11.pas
@@ -0,0 +1,8 @@
+﻿var
+  s: string;
+  i: byte;
+begin
+  readln(s);
+  for i:= 1 to length(s) do
+  write(s[i],' ');
+end.
---
+++ PascalABC/string/string 19.pas
@@ -0,0 +1,17 @@
+﻿var
+a: string;
+b: integer;
+c: byte;
+begin
+  writeln('');
+  readln(a);
+  c:= 1;
+  for b := 1 to length (a) do
+    if (a[b] < '0') or (a[b] > '9') then
+      if (c = 1) and ( a[b] = '.') then
+        c:= 2
+        else
+        c:= 0;
+        writeln(c);
+        readln       
+end.
