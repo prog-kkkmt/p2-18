df145dcdfe1566eb666f2ca642a23ba2a39160d9
Mental41 <64136709+Mental41@users.noreply.github.com>
2021-02-11 11:16:30 +0300

    Add files via upload

---
+++ Pascal/text/test5.pas
@@ -0,0 +1,14 @@
+﻿// Дана строка S и текстовый файл. Добавить строку S в конец файла.
+var
+f:text;
+s:string;
+begin
+  assign(f,'test5.txt');
+  reset(f);
+  while not eof(f) do
+  begin
+    readln(f,s);
+    writeln(s);
+  end;
+  close(f);
+end.
---
+++ Pascal/text/text13.pas
@@ -0,0 +1,24 @@
+﻿// Дан непустой текстовый файл. Удалить из него первую строку.
+const MaxN=1000;
+var
+a:array[1..MaxN] of string;
+i,n:integer;
+s:string;
+f:text;
+text13:string;
+begin
+  readln(text13);
+  assign(f,text13);
+  reset(f);
+  n:=0;
+  while not (eof(f)) do
+  begin
+    n:=n+1;
+    readln(f,a[n]);
+  end;
+  close(f);
+  rewrite(f);
+  for i:=2 to n do
+    writeln(f,a[i]);
+    close(f);
+end.
