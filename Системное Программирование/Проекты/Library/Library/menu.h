1e1454d5b173f37488a684e9af30c99ce2e91651
Иван <kvar59@yandex.ru>
2021-02-23 10:47:21 +0300

    :D

--- Системное Программирование/Проекты/Library/Library/menu.h
+++
@@ -1,6 +0,0 @@
-#ifndef MENU_H_INCLUDED
-#define MENU_H_INCLUDED
-
-int menu();
-
-#endif // MENU_H_INCLUDED
--- Системное Программирование/Проекты/main/sort_viborom.h
+++
@@ -1,6 +0,0 @@
-#ifndef FIND_IN_LIST_H_INCLUDED
-#define FIND_IN_LIST_H_INCLUDED
-
-void sort_viborom();
-
-#endif // FIND_IN_LIST_H_INCLUDED
