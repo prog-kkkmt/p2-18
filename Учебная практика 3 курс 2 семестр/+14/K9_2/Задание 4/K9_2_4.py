898e61abd5545363e9ba1bedef5e379beb60f7c4
SparkieAG <64137055+SparkieAG@users.noreply.github.com>
2021-04-28 11:52:13 +0300

    Add files via upload

---
+++ Учебная практика 3 курс 2 семестр/+14/K9_2/Задание 4/K9_2_4.py
@@ -0,0 +1,25 @@
+#Выполнил: Кузнецов Матвей
+#Группа: П2-18
+'''
+К9_2. Техника работы с циклом for и генераторами списков
+
+Задание 4. Matrix3. Даны целые положительные числа M, N и набор из M чисел. Сформировать
+матрицу размера M x N, у которой в каждом столбце содержатся все числа из исходного
+набора (в том же порядке).
+'''
+
+from random import randint
+
+m = int(input())
+n = int(input())
+
+a = [randint(1, 10) for i in range(m)]
+print(a)
+mtrx1 = []
+for i in range(m):
+    list_ = []
+    for j in range(n):
+        list_.append(a[i])
+    mtrx1.append(list_)
+
+[print(' '.join([str(j) for j in i])) for i in mtrx1]
