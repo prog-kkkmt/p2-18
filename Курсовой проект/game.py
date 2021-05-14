75be6e784530fb5ee44cd44c74a50a61711a9334
dartin31 <64136541+dartin31@users.noreply.github.com>
2021-05-14 17:38:16 +0300

    Add files via upload

--- Курсовой проект/game.py
+++ Курсовой проект/game.py
@@ -1,125 +1,96 @@
 from tkinter import *
+from PIL import ImageTk, Image
+
+money = 1
+upgrade = 1
 
 
 def main():
 
-    def click():
-        money = 0
-        money = money+1
-        box2.insert(END, money)
+    def upgradeCPU():  
+        global money
+        global upgrade
+        if money >= 50:
+            money -= 50
+            upgrade += 5
+        UpgradeText.set("{}$".format(upgrade))
+        MoneyText.set("{}$".format(money))
 
+
+    def click():  
+        global money
+        global upgrade
+        money += upgrade
+        MoneyText.set("{}$".format(money))
     
-
-    def mainwindow1():
-
-        def add1():
-            box1.insert(END, en1.get() + ' ' + en2.get())
-            en1.delete(0, END)
-            en2.delete(0, END)
-
-        def change1():
-            select = list(box1.curselection())
-            select.reverse()
-            for i in select:
-                box1.delete(i)
-                box1.insert(i, en1.get() + ' ' + en2.get())
-            en1.delete(0, END)
-            en2.delete(0, END)
-
-        def remove1():
-            select = list(box1.curselection())
-            select.reverse()
-            for i in select:
-                box1.delete(i)
-
-        def save1():
-            f = open('prof.txt', 'w')
-            f.writelines("\n".join(box1.get(0, END)))
-            f.close()
-
-
-
-
-        # Создание объекта окна верхнего уровня (на нём будут распологаться все элементы
-        root = Tk()
-        # Задание размера окна и его координат расположения
-        root.geometry("300x230+700+300")
-        root["bg"] = "gray22"
-
-
-        # Задаём местоположение текстовой метке и полю для ввода
-        lb1 = Label(root,text="Код должности:", background="gray22", foreground="#ccc")
-        lb1.place(x=2, y=16)
-        en1 = Entry(root, width=15)
-        en1.pack()
-        en1.place(x=100, y=18)
-
-        # Задаём местоположение текстовой метке и полю для ввода
-        lb2 = Label(root,text="Название:", background="gray22", foreground="#ccc")
-        lb2.place(x=15, y=48)
-        en2 = Entry(root, width=15)
-        en2.pack()
-        en2.place(x=100, y=50)
-
-        lb3 = Label(root,text="База данных:", background="gray22", foreground="#ccc")
-        lb3.place(x=15, y=130)
-        box1 = Listbox(root, selectmode=EXTENDED, width=15, height=8)
-        box1.place(x=100, y=80)
-        scroll1 = Scrollbar(root, command=box1.yview)
-        scroll1.pack(side=RIGHT, fill=Y)
-        box1.config(yscrollcommand=scroll1.set)
-
-        b1 = Button(root, text="Добавить",background="#555", foreground="#ccc", width=10, height=1, command=add1)
-        b1.place(x=200, y=87)
-        b2 = Button(root, text="Изменить",background="#555", foreground="#ccc", width=10, height=1, command=change1)
-        b2.place(x=200, y=117)
-        b3 = Button(root, text="Удалить",background="#555", foreground="#ccc", width=10, height=1, command=remove1)
-        b3.place(x=200, y=147)
-        b4 = Button(root, text="Сохранить",background="#555", foreground="#ccc", width=10, height=1, command=save1)
-        b4.place(x=200, y=177)
-
-        with open('prof.txt', 'r') as f:
-            lst = f.readlines()
- 
-        for item in lst:
-            box1.insert(END, item)
+    def save1():
+        f = open('prof.txt', 'w')
+        f.writelines("\n".join(box1.get(0, END)))
+        f.close()
 
     # Создание объекта окна верхнего уровня (на нём будут распологаться все элементы
     mainwindow = Tk()
     # Задание размера окна и его координат расположения
-    mainwindow.geometry("315x240+700+300")
+    mainwindow.geometry("530x340+700+300")
+    mainwindow.title("Симулятор программиста - Дом")
     mainwindow["bg"] = "gray22"
     mainmenu = Menu(mainwindow)
     mainwindow.config(menu=mainmenu)
     helpmenu = Menu(mainmenu, tearoff=0)
     helpmenu2 = Menu(helpmenu, tearoff=0)
-    mainmenu.add_cascade(label="О программе",menu=helpmenu)
-    helpmenu.add_cascade(label="Справка",menu=helpmenu2)
-    helpmenu2.add_command(label="Открыть сайт",)
+    mainmenu.add_cascade(label="О игре",menu=helpmenu)
+    helpmenu.add_cascade(label="Информация",menu=helpmenu2)
+    helpmenu2.add_command(label="Версия v0.1",)
+    helpmenu2.add_separator()
+    helpmenu2.add_command(label="Секретный код - GE5GS",)
     helpmenu.add_separator()
     helpmenu.add_command(label="Выход",)
 
-    lb1 = Label(mainwindow, text="Деньги:", background="gray22", foreground="#ccc")
-    lb1.place(x=20, y=10)
-    lb1 = Label(mainwindow, text="Симулятор программиста", background="gray22", foreground="#ccc")
-    lb1.place(x=80, y=40)
-    b1 = Button(mainwindow, text="Работать", background="#555", foreground="#ccc", width=15, height=1, command=click)
-    b1.place(x=100, y=70)
-    b1 = Button(mainwindow, text="Персонаж", background="#555", foreground="#ccc", width=15, height=1, command=mainwindow1)
-    b1.place(x=100, y=100)
-    b1 = Button(mainwindow, text="Магазин", background="#555", foreground="#ccc", width=15, height=1, command=mainwindow1)
-    b1.place(x=100, y=130)
-    b1 = Button(mainwindow, text="Сохранить и выйти", background="#555", foreground="#ccc", width=15, height=1, command=mainwindow1)
-    b1.place(x=100, y=160)
-    box2 = Listbox(mainwindow, selectmode=EXTENDED, width=10, height=1)
-    box2.place(x=70, y=12)
+    MoneyText = StringVar()
+    MoneyText.set("{}$".format(upgrade))
+    UpgradeText = StringVar()
+    UpgradeText.set("{}$".format(upgrade))
+    
+    img = ImageTk.PhotoImage(Image.open("img\comp.png"))
+    img2 = ImageTk.PhotoImage(Image.open("img\cart.png"))
+    img3 = ImageTk.PhotoImage(Image.open("img\money.png"))
+    img4 = ImageTk.PhotoImage(Image.open("img\shop\cpu.png"))
+    img5 = ImageTk.PhotoImage(Image.open("img\shop.png"))
+
+    MoneyEntry = Label(mainwindow, image=img3, background="gray22")
+    MoneyEntry.place(x=190, y=12)
+    ShopEntry = Label(mainwindow, image=img5, background="gray22")
+    ShopEntry.place(x=10, y=123)
+    lb1 = Label(mainwindow, textvariable=MoneyText, font = ('Arial' , 40), background="#414141", foreground="#ccc")
+    lb1.place(x=200, y=14)
+    lb2 = Label(mainwindow, text="Заработок за нажатие:", background="#414141", foreground="#ccc")
+    lb2.place(x=200, y=70)
+    lb1 = Label(mainwindow, textvariable=UpgradeText, background="#414141", foreground="#ccc")
+    lb1.place(x=329, y=70)
+    lb3 = Label(mainwindow, text="Магазин", background="gray22", foreground="#ccc")
+    lb3.place(x=85, y=89)
+    b2 = Button(mainwindow, background="#414141", relief = 'flat', image=img4, command=upgradeCPU)
+    b2.place(x=30, y=130)
+    lb3 = Label(mainwindow, text="CPU", background="#414141", foreground="#ccc")
+    lb3.place(x=43, y=190)
+    lb3 = Label(mainwindow, text="Цена: 50$", background="#414141", foreground="#ccc")
+    lb3.place(x=30, y=210)
+    lb3 = Label(mainwindow, text="Заработок: 5$", background="#414141", foreground="#ccc")
+    lb3.place(x=20, y=230)
+    b4 = Label(mainwindow, background="gray22", image=img2)
+    b4.place(x=54, y=83)
+    b3 = Button(mainwindow, text="Сохранить и выйти", relief = 'flat', background="#555", foreground="#ccc", width=15, height=1)
+    b3.place(x=148, y=280)
+    b3 = Button(mainwindow, text="Ввести код", relief = 'flat', background="#555", foreground="#ccc", width=15, height=1)
+    b3.place(x=28, y=280)
+    clickbtn = Button(mainwindow, background="gray22", relief = 'flat', image=img, command=click)
+    clickbtn.place(x=280, y=100)
 
 
     
 
     # Запуск цикла обработки событий
     mainloop()
-
 
 
 if __name__ == '__main__':
