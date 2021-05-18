53fa1f0516d024274f2aa5e670c95faa069a931f
DelcorvuZZ <64140846+DelcorvuZZ@users.noreply.github.com>
2021-05-18 16:56:30 +0300

    Add files via upload

---
+++ Учебная практика/3 раздел/list.py
@@ -0,0 +1,46 @@
+from tkinter import *
+import sqlite3
+
+root = Tk()
+root.title("form")
+
+root.geometry('600x400')
+
+
+
+
+name_label = Label(root, text="Name")
+name_label.grid(row=0, column=0)
+
+name_entry = Entry(width=30)
+name_entry.grid(row=0, column=1)
+
+surname_label = Label(root, text="Surname")
+surname_label.grid(row=1, column=0)
+
+surname_entry = Entry(width=30)
+surname_entry.grid(row=1, column=1)
+
+country_label = Label(root, text="Country")
+country_label.grid(row=2, column=0)
+
+country_entry = Entry(width=30)
+country_entry.grid(row=2, column=1)
+
+index_label = Label(root, text="Index")
+index_label.grid(row=3, column=0)
+
+index_entry = Entry(width=30)
+index_entry.grid(row=3, column=1)
+
+def take_all():
+    name = name_entry.get()
+    surname = surname_entry.get()
+    country = country_entry.get()
+    index = index_entry.get()
+    print(name, surname, country, index)
+
+add_button = Button(text="Add", width=10, command=take_all)
+add_button.grid(row=4, column=1)
+
+root.mainloop()
