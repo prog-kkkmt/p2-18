dc96c857831c0bcd9b014aec82725973fad16711
emptyzach <64543798+emptyzach@users.noreply.github.com>
2021-05-22 14:50:22 +0300

    Update for5.py

--- Учебная практика/Практическое занятие 5/for5.py
+++ Учебная практика/Практическое занятие 5/for5.py
@@ -1,6 +1,8 @@
-#https://pythontutor.ru/lessons/for_loop/problems/sum_of_ten_numbers/
-#Дано 10 целых чисел. Вычислите их сумму.
-#Напишите программу, использующую наименьшее число переменных.
+# Выполнил: Бурлаев З. С.
+# https://pythontutor.ru/lessons/for_loop/problems/sum_of_ten_numbers/
+# Дано 10 целых чисел. Вычислите их сумму.
+# Напишите программу, использующую наименьшее число переменных.
+
 sum = 0
 for i in range(10):
     number = int(input())
