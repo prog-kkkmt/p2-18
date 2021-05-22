12ec4b42bbcd8b06d7d1ca8da9c3b0c2a3181e8a
emptyzach <64543798+emptyzach@users.noreply.github.com>
2021-05-22 14:57:26 +0300

    Update while3.py

--- Учебная практика/Практическое занятие 5/while3.py
+++ Учебная практика/Практическое занятие 5/while3.py
@@ -1,7 +1,9 @@
-#https://pythontutor.ru/lessons/while/problems/minimal_divisor/
-#Дано целое число, не меньшее 2.
-#Выведите его наименьший натуральный делитель,
-#отличный от 1.
+# Выполнил: Бурлаев З. С.
+# https://pythontutor.ru/lessons/while/problems/minimal_divisor/
+# Дано целое число, не меньшее 2.
+# Выведите его наименьший натуральный делитель,
+# отличный от 1.
+
 n = int(input())
 i = 2
 while n % i != 0:
