e97ec0b4823dbcf5652c2d9eea8be5dab45df36b
emptyzach <64543798+emptyzach@users.noreply.github.com>
2021-05-23 22:20:21 +0300

    Update main.py

--- Учебная практика/Практическое занятие 22/main.py
+++ Учебная практика/Практическое занятие 22/main.py
@@ -1,27 +1,33 @@
+# Выполниили: Бурлаев Захар, Воронцов Александр
+
+"""
+Данная программа создает приложение с графическим интерфейсом, приложение предоставляет возможность выбрать папку и просмотреть все файлы находящиеся в ней.
+"""
+
 from PyQt5 import QtWidgets
-import sys
+import sys # sys нужен для передачи argv в QApplication
 import os
-import app1
+import app1 # Это наш конвертированный файл дизайна
 
 class ExampleApp(QtWidgets.QMainWindow, app1.Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.browse_folder)
-
+        self.setupUi(self) # Это нужно для инициализации нашего дизайна
+        self.pushButton.clicked.connect(self.browse_folder)# Выполнить функцию browse_folder при нажатии кнопки
+        
     def browse_folder(self):
-        self.listWidget.clear()
+        self.listWidget.clear() # На случай, если в списке уже есть элементы
         directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
 
-        if directory:
-            for file_name in os.listdir(directory):
-                self.listWidget.addItem(file_name)
+        if directory: # не продолжать выполнение, если пользователь не выбрал директорию
+            for file_name in os.listdir(directory): # для каждого файла в директории
+                self.listWidget.addItem(file_name) # добавить файл в listWidget
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = ExampleApp()
-    window.show()
-    app.exec_()
+    window = ExampleApp() # Создаём объект класса ExampleApp
+    window.show() # Показываем окно
+    app.exec_() # запускаем приложение
 
 if __name__ == '__main__':
     main()
