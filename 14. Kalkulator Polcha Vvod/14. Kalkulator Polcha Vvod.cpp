749b795343c095a65165d368c4d1eb893a97f460
Mental41 <64136709+Mental41@users.noreply.github.com>
2022-02-25 12:00:12 +0300

    Add files via upload

---
+++ 14. Kalkulator Polcha Vvod/14. Kalkulator Polcha Vvod.cpp
@@ -0,0 +1,59 @@
+﻿// 14. Kalkulator Polcha Vvod.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "14. Kalkulator Polcha Vvod.h"
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+
+    int main()
+    {
+        int stack[10];
+        int sp = 0;
+        printf("enter retern Poland expression");
+        while (!feof(stdin))
+        {
+            int c = getchar();
+            int x;
+            switch(c)
+            {
+            case ' ':
+            case '\n':
+                break;
+            case '=':
+                printf("My Result = %d\n", stack[sp - 1]); sp--;
+                break;
+            case '+':
+                stack[sp - 2] = stack[sp - 2] + stack[sp - 1]; sp--;
+                break;
+            case '-':
+                stack[sp - 2] = stack[sp - 2] - stack[sp - 1]; sp--;
+                break;
+            case '*':
+                stack[sp - 2] = stack[sp - 2] * stack[sp - 1]; sp--;
+                break;
+            case '/':
+                stack[sp - 2] = stack[sp - 2] / stack[sp - 1]; sp--;
+                break;
+            case '^':
+                stack[sp - 1] = pow (2,4); sp--;
+                break;
+            default:
+                ungetc(c, stdin);
+                if (scanf("%d", &x) != 1)
+                {
+                    fprintf(stderr, "Can't read integer\n");
+                    return -1;
+                }
+                else
+                {
+                    stack[sp] = x; sp++;
+                }
+            }
+        }
+        printf("Result = %d\n", stack[sp - 1]);
+        return 0;
+    return 0;
+    }
+    
